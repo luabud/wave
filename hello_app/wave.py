a2514629a5b1eddae5dff838c81acd0b0629769d
Luciana Abud <luabud@microsoft.com>
2019-09-12 10:58:59 -0700

    change interval

--- hello_app/wave.py
+++ hello_app/wave.py
@@ -1,13 +1,13 @@
 from math import cos, radians
 
 def make_dot_string(x):
-    rad = radians(x)  
+    rad = radians(x) 
     c = abs(round(cos(rad),2))
-    numspaces = int((10 *1/c - 10)) 
+    numspaces = int((9 *1/c  - 9)) 
     return numspaces
 
 def print_wave(y=1800):
-    for i in range(0, y, 10):
+    for i in range(0, y, 3):
         numspaces= make_dot_string(i)
         print(' ' * numspaces + 'o')
 
