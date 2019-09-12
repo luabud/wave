41f2b03335686621a4729537dbfe9172d583ebf1
Luciana Abud <luabud@microsoft.com>
2019-09-12 10:07:57 -0700

    add pytest tests

--- hello_app/wave.py
+++ hello_app/wave.py
@@ -1,15 +1,14 @@
 from math import cos, radians
 
-def make_dot_string(x,div=1800):
+def make_dot_string(x):
     rad = radians(x)  
     c = abs(round(cos(rad),2))
     numspaces = int((10 *1/c - 10)) 
     return numspaces
 
 def print_wave(y=1800):
-    div = y
     for i in range(0, y, 10):
-        numspaces= make_dot_string(i,div)
+        numspaces= make_dot_string(i)
         print(' ' * numspaces + 'o')
 
 if __name__ == '__main__':
