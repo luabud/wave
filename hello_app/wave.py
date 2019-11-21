82405f5d940454a4de422eaaa99f8ea285a53fd6
luabud <luabud@microsoft.com>
2019-11-20 19:03:47 -0800

    set up tools for vscode

--- hello_app/wave.py
+++ hello_app/wave.py
@@ -2,10 +2,10 @@ from math import cos, radians
 
 def make_dot_string(x):
     rad = radians(x) 
-    c = abs(round(cos(rad),2))
-    if c==0:
+    c = 1 / abs(round(cos(rad), 2))
+    if c == 0:
         return 0
-    numspaces = int((9 *1/c  - 9)) 
+    numspaces = int((9 *c  - 9)) 
     return numspaces
 
 def print_wave(y=1800):
@@ -15,6 +15,5 @@ def print_wave(y=1800):
 
 if __name__ == '__main__':
     import sys
-    num=int(sys.argv[1])
-
+    num = int(sys.argv[1])
     print_wave(num)
