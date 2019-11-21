c5952b57d6cef404d5c172b495a5627e42039168
luabud <luabud@microsoft.com>
2019-11-20 19:23:38 -0800

    set up pythonpath

--- hello_app/wave.py
+++ hello_app/wave.py
@@ -2,10 +2,10 @@ from math import cos, radians
 
 def make_dot_string(x):
     rad = radians(x) 
-    c = 1 / abs(round(cos(rad), 2))
+    c = abs(round(cos(rad), 2))
     if c == 0:
         return 0
-    numspaces = int((9 *c  - 9)) 
+    numspaces = int((9 *1/c  - 9)) 
     return numspaces
 
 def print_wave(y=1800):
